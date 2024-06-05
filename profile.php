622dce473ee1210fb2ae18785917a273249b40a9
ridho6 <muhammad.ridho.syahputra18@gmail.com>
2024-06-05 13:48:37 +0800

    praktikum-web2

---
+++ profile.php
@@ -0,0 +1,46 @@
+<!-- <?php include "header.php" ?> -->
+
+<div class="row">
+  <div class="col-md-12">
+    <div class="text-center">
+      <img src="img/ridho.jpg" alt="Ridho Syahputra" width="160" height="185" class="rounded-circle">
+      <table class="table mt-4" style="width:100%; margin:0 auto;">
+        <tbody>
+          <tr>
+            <td align="right" width="50%"><strong>NIK / NPM :</strong></td>
+            <td align="left" width="50%">2210010533 / 2210010533</td>
+          </tr>
+          <tr>
+            <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+            <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+          </tr>
+          <tr>
+            <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+            <td align="left" width="50%">Muhammad Ridho Syahputra</td>
+          </tr>
+          <tr>
+            <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+            <td align="left" width="50%">Kotabaru, 06 Januari 2004</td>
+          </tr>
+          <tr>
+            <td align="right" width="50%"><strong>Telepon :</strong></td>
+            <td align="left" width="50%">0823 5274 7908</td>
+          </tr>
+          <tr>
+            <td align="right" width="50%"><strong>email :</strong></td>
+            <td align="left" width="50%">muhammad.ridho.syahputra18@gmail.com</td>
+          </tr>
+          <tr>
+            <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+            <td align="left" width="50%">
+              <a href="http://instagram.com/_ridho_s_" target="_blank" style="text-decoration:none;">
+                <img src="img/medsos/instagram.svg" width="30" alt="instagram">
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+<?php include "footer.php" ?>
